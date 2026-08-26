86f5f1cd1d7c9504c4edf8e2703d0ea5c64d1050
tsudevtak0 <dev.tsu.tak0@gmail.com>
2026-08-26 14:57:18 +0900

    Add files via upload

---
+++ main.ps1
@@ -0,0 +1,14 @@
+﻿# ==========================================
+# テスト
+# ==========================================
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show(
+    "Hello World!",                  # 表示するメッセージ
+    "PowerShell 通知",               # ウィンドウのタイトル
+    [System.Windows.Forms.MessageBoxButtons]::OK, # ボタンの種類（OKボタンのみ）
+    [System.Windows.Forms.MessageBoxIcon]::Information # アイコン（情報アイコン）
+)
+
+# コンソール側にもメッセージを出力する場合
+Write-Host "ポップアップを表示しました。" -ForegroundColor Green
